e44cb1cc57191b135efb6815e140e17e9ac26a59
mohinitupe2005 <mohinitupe2005@gmail.com>
2026-02-12 06:55:34 +0000

    Added JS basic variable, datatype and conversion files

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,8 @@
+const accountId = 1255543
+let accountEmail = "mohini@gmail.com"
+var accountPass = "12345"
+accountCity = "Jaipur"
+
+
+/*avoid using var due to validity and scopr of the var*/ 
+console.table([accountId, accountEmail, accountPass, accountCity])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat sll js code as newer version
+
+//alert(3+3)   //were using node.js not browser
+
+console.log(3+3) //code readability should be high
+console.log("Mohini")
+
+let name = "Mohini"
+let age = 18
+let isLoggedIn = false
+let state;
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true or false
+// null => standalone value
+// undefined => value not defined
+// symbol => unique
+
+
+// object
+
+console.log(typeof null);   //null is an object datatype
