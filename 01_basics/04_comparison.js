0b48178329620dccde9036c0089d44f5534d2345
mohinitupe2005 <mohinitupe2005@gmail.com>
2026-02-12 16:00:40 +0000

    Added JS basic variable, datatype and conversion files

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,26 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);   //datatype should be same in comparison
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+//avoid null and undefined as they create confusion
+
+
+// === strict check
+console.log("2" === 2);
+
+
+
+
+
