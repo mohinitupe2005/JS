30bcb461e2d755605e06eadcf1b043cbfa157e5e
mohinitupe2005 <mohinitupe2005@gmail.com>
2026-02-13 13:23:56 +0000

    add

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,69 @@
+//const tinderUser = new Object()
+
+const tinderUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false
+
+//console.log(tinderUser)
+
+const regularUser = {
+    email : "some@gmail.com",
+    fullname: {
+        userfullname:{
+            firstname: "mohini",
+            lastname: "tupe"
+        }
+    }
+}
+
+//console.log(regularUser.fullname.userfullname.lastname);
+
+const obj1 ={
+    1:"a",
+    2: "b"
+}
+const obj2 = {3: "c", 4:"d"}
+
+//const obj3 = {obj1, obj2}
+
+//const obj3 = Object.assign({}, obj1, obj2)    //ho ssakta hai aur bi values ho uske liye easy hai {} lagana
+
+const obj3 = {...obj1, ...obj2}        //spread operator
+  
+//console.log(obj3);
+
+const users = [
+    {
+        id:1,
+        email: "h@gmail.com"
+    },
+    {
+        id:1,
+        email: "h@gmail.com"
+    },
+    {
+        id:1,
+        email: "h@gmail.com"
+    },
+    {
+        id:1,
+        email: "h@gmail.com"
+    },
+    {
+        id:1,
+        email: "h@gmail.com"
+    },
+    {
+        id:1,
+        email: "h@gmail.com"
+    },
+    {
+        id:1,
+        email: "h@gmail.com"
+    }
+]
+
+users[1].email
+console.log(tinderUser)
