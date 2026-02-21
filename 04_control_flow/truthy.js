23e693138babaf269b65605d70370c0d7b4ec1b5
mohinitupe2005 <mohinitupe2005@gmail.com>
2026-02-21 14:45:26 +0000

    commit

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,41 @@
+const userEmail = []
+
+if(userEmail){
+    console.log("User email");
+}else{
+    console.log("Don't have user email");
+}
+
+
+//falsy value
+// false, 0, -0, BigInt 0n, "", null, undefined, Nan
+
+//truthy
+//"0"(string ke andar 0), 'false', " ", [], {}, function(){}  (empty function)
+
+// if(userEmail.length === 0){
+//     console.log("Array is empty");
+// }
+
+const emptyObj = {}
+
+if(Object.keys(emptyObj).length === 0){
+    console.log("object is empty");
+}
+
+//Nullish Coalescing Operator(??) : Null undefined
+
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 15
+val1 = null ?? 10 ?? 20
+
+console.log(val1);
+
+//Terniary operator
+
+// condition ? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log("less than 80 ") : console.log("more than 80")
