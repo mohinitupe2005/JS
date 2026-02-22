d89a36425e083e05b55469cfb97156171bf9949b
mohinitupe2005 <mohinitupe2005@gmail.com>
2026-02-22 09:13:05 +0000

    add

---
+++ 05_iteration/eight.js
@@ -0,0 +1,32 @@
+const myNums = [1, 2, 3]
+
+// const myTotal = myNums.reduce(function (acc, currval) {
+//     console.log(`acc : ${acc} and currval : ${currval}`); 
+//     return acc + currval
+// }, 0)
+
+const myTotal = myNums.reduce( (acc, curr) => acc+curr, 0)
+
+console.log(myTotal);
+
+const shoppingcart = [
+    {
+        itemname : "js course",
+        price : 2999
+    },
+    {
+        itemname : "py course",
+        price : 999
+    },
+    {
+        itemname : "mobile dev course",
+        price : 5999
+    },
+    {
+        itemname : "data scientist course",
+        price : 12999
+    }
+]
+
+const priceToPay = shoppingcart.reduce( (acc,item) => acc +(item.price),0)
+console.log(priceToPay);
